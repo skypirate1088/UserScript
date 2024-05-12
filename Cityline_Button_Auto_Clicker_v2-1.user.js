9899cc5388ce94c60ff587d043bd77ac4a4070f0
skypirate1088 <122192510+skypirate1088@users.noreply.github.com>
2024-05-13 00:17:08 +0800

    Add files via upload

---
+++ Cityline_Button_Auto_Clicker_v2-1.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Cityline Button Auto Clicker v2
+// @namespace    YourNamespace
+// @version      1
+// @description  Monitors the "前往購票" button and clicks it when it appears
+// @match        https://*.cityline.com/*
+// @grant        none
+// ==/UserScript==
+
+// This is a hypothetical script and may not work on all websites.
+// Replace 'buyTicketBtn' with the actual ID of the button on the website.
+
+(function() {
+    'use strict';
+
+    // Function to check for button availability and click
+    function clickBuyTicketButton() {
+        // Find the button by ID
+        var buyButton = document.getElementById('buyTicketBtn');
+
+        // If the button is found and it is not disabled
+        if (buyButton && !buyButton.disabled) {
+            console.log('Button found and clicked!');
+            // Click the button
+            buyButton.click();
+        } else {
+            console.log('Button not available yet, retrying in 1 second...');
+            // Retry after 1 second if the button is not available
+            setTimeout(clickBuyTicketButton, 1000);
+        }
+    }
+
+    // Start the process
+    clickBuyTicketButton();
+})();
